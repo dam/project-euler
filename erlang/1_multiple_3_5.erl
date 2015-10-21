27a8ec5dd51d209c72a341c402de104cec0d39e5
Damien Imberdis <imberdis.damien@gmail.com>
2015-10-21 16:26:57 -0400

    adding erlang solution

---
+++ erlang/1_multiple_3_5.erl
@@ -0,0 +1,25 @@
+#!/usr/bin/env escript
+
+main([String]) ->
+  try
+  	N = list_to_integer(String),
+  	statistics(runtime),
+  	Result = original(N),
+  	{_, Time} = statistics(runtime),
+  	io:format("Result: ~w in ~p milliseconds~n", [Result, Time])
+  catch
+  	_:_ ->
+  	  usage()
+  end;
+
+main(_) ->
+  usage().
+
+usage() ->
+  io:format("usage: 1_multiple_3_5 1000"),
+  halt(1).
+
+% Functional code
+original(Number) ->
+  L = [X || X <- lists:seq(1, Number - 1), (X rem 3 =:= 0) or (X rem 5 =:= 0)],
+  lists:sum(L).
